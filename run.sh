5d89714e202d7ccfa854578cf21dddf3695129c7
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-10-06 18:01:58 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -o stego src/writers/* src/readers/* src/stego.c -lssl -lcrypto
+make DEBUG=1
 
-./stego -embed -in hello.txt -p mesi.bmp -out out.bmp
+./bin/stego -embed -in hello.txt -p mesi.bmp -out out.bmp
