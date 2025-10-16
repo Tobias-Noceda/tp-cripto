04a201862c832df442cbd59014552e3f3a3c6465
Tobias-Noceda <tnoceda@itba.edu.ar>
2025-10-16 22:33:10 +0000

    Improved run.sh

--- run.sh
+++ run.sh
@@ -6,9 +6,14 @@ stg=$3
 make clean all DEBUG=1
 
 extension=${msg##*.}
+if [ "$extension" = "$msg" ]; then
+    extension=""
+else 
+    extension=".$extension"
+fi
 
 ./bin/stego -embed -p $img -in $msg -out out.bmp -stego $stg
 ./bin/stego -extract -p out.bmp -out extracted -stego $stg
 
 echo
-diff $msg extracted.$extension
+diff $msg extracted$extension
