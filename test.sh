fa3716e2ef3f87fb2ceaf85422751a25c77d336e
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-10-22 19:40:26 -0300

    Add embed unit tests

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+fails=0
+
+for stego in LSB1 LSB4 #LSBI
+do
+    ./test-embed.sh $stego || ((fails++))
+    ./test-extract.sh $stego || ((fails++))
+done
+
+if [ $fails -ne 0 ]
+then
+    false
+fi
